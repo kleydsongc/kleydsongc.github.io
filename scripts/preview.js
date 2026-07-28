6a4154a8ddfd8ca37fe041cf020967fc8333b3f9
kleydson <57597284+kleydsongc@users.noreply.github.com>
2026-07-28 13:22:06 -0300

    Implementação de javascript
    Script js para aplicação de funcionalidade de preview/visualização dos projetos e demais páginas do portfólio.

---
+++ scripts/preview.js
@@ -0,0 +1,18 @@
+const preview = document.getElementById("preview-img");
+const projetos = document.querySelectorAll("[data-preview]");
+
+projetos.forEach(function(projeto) {
+	projeto.addEventListener("mouseenter", function() {
+		preview.src = "assets/gifs/static.gif";
+		setTimeout(() => {
+			preview.src = projeto.dataset.preview;
+		}, 150);
+	});
+
+	projeto.addEventListener("mouseleave", function() {
+		preview.src = "assets/gifs/static.gif";
+		setTimeout(() => {
+			preview.src = "assets/gifs/dvd.gif";
+		}, 150);
+	});
+});
